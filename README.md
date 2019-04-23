4523373ff7163721dbf93d2b4a6520dbbb194103
Michael <Michael.Fornaro@anz.com>
2019-04-23 12:54:11 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Kube-Compose - Homebrew Tap
+
+The download artifact must match the SHA sum within homebrew, to update the SHA sum within the homebrew tap run the following command:
+
+```bash
+shasum -a 256 <file>
+```
--- kube-compose.rb
+++ kube-compose.rb
@@ -1,9 +1,9 @@
 class "Kube-Compose" < Formula
     desc "A command line tool to enable compose functionality on Kubernetes"
     homepage "https://github.com/kube-compose/kube-compose"
-    url "https://github.com/kube-compose/kube-compose/releases/download/v0.1.0/darwin-proxy.tar.gz"
-    sha256 "471b24696ce98287ab3eb0933da87b8f3202f5a23ee910d3c702babaf0056bf8"
-    version "0.1.0"
+    url "https://github.com/jbrekelmans/kube-compose/releases/download/3.0.3/kube-compose.darwin.tar.gz"
+    sha256 "ac19d8fa328d324241f2ebc3b6837055f26d955f483fe675f891ccd14d5b75d5"
+    version "3.0.3"
   
     bottle :unneeded
   
