7a663d474ff2f774241ceae96862b7309230aa42
Jasper Brekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-06-03 20:32:08 +1000

    Bump version properly

--- kube-compose.rb
+++ kube-compose.rb
@@ -1,7 +1,7 @@
 class KubeCompose < Formula
     desc "A command line tool to enable compose functionality on Kubernetes"
     homepage "https://github.com/kube-compose/kube-compose"
-    url "https://github.com/jbrekelmans/kube-compose/releases/download/0.4.1/kube-compose.darwin.tar.gz"
+    url "https://github.com/jbrekelmans/kube-compose/releases/download/0.5.0/kube-compose.darwin.tar.gz"
     sha256 "d92e693e789f39c56cd90821481151cfe7e6c891b6db0dd1ced017023562fb60"
     version "0.5.0"
   
