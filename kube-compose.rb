fe230245768b006f099df559ed2c1663dcf94ca9
Jasper Brekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-06-04 19:00:45 +1000

    update descr

--- kube-compose.rb
+++ kube-compose.rb
@@ -1,5 +1,5 @@
 class KubeCompose < Formula
-    desc "kube-compose can create and destroy environments in Kubernetes based on docker compose files designed for CI"
+    desc "kube-compose can create and destroy environments in Kubernetes based on docker compose files with an emphasis on CI use cases"
     homepage "https://github.com/kube-compose/kube-compose"
     url "https://github.com/kube-compose/kube-compose/releases/download/v0.5.0/kube-compose.darwin.tar.gz"
     sha256 "d92e693e789f39c56cd90821481151cfe7e6c891b6db0dd1ced017023562fb60"
