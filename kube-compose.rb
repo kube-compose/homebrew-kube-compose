cddbfab9cf148f056d20341ef123c2f7b31dd6c5
Jasper Brekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-06-20 03:56:34 +1000

    Bump version to 0.5.2

--- kube-compose.rb
+++ kube-compose.rb
@@ -1,9 +1,9 @@
 class KubeCompose < Formula
     desc "kube-compose can create and destroy environments in Kubernetes based on docker compose files with an emphasis on CI use cases"
     homepage "https://github.com/kube-compose/kube-compose"
-    url "https://github.com/kube-compose/kube-compose/releases/download/v0.5.1/kube-compose.darwin.tar.gz"
-    sha256 "7d5a8a31d022da3471c052a544835e4163c7050c43278fe12e15c8065227074f"
-    version "0.5.1"
+    url "https://github.com/kube-compose/kube-compose/releases/download/v0.5.2/kube-compose.darwin.tar.gz"
+    sha256 "98a66073c3518069bcb5a8d088dae14a0a92a5b51e3e790b1180255fc8aa3de8"
+    version "0.5.2"
   
     bottle :unneeded
   
